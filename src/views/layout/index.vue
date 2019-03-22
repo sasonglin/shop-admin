b74c33f762d35aae77c4b7bce606fcca530608b8
sasonglin <sasonglinzb@163.com>
2019-03-22 19:28:58 +0800

    进一步修改

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,60 @@
 <template>
   <div class="content">
     <el-container>
-    <el-header>Header</el-header>
+    <el-header>
+    <el-row>
+      <el-col :span="4"><div class="grid-content bg-purple">
+        <img src="./logo.png" alt="">
+        </div>
+        </el-col>
+      <el-col :span="18"><div class="grid-content bg-purple">黑马程序员</div></el-col>
+      <el-col :span="2"><div class="grid-content bg-purple">
+        <a href="#">退出</a>
+        </div></el-col>
+    </el-row>
+    </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="fas fa-user-secret"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -12,7 +63,14 @@
 
 <script>
 export default {
-
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
 }
 </script>
 
@@ -31,5 +89,13 @@ export default {
 }
 .el-aside {
   background-color: #d3dce6;
+  height: 100%;
 }
+.el-menu {
+  height: 100%;
+}
+.el-col {
+  line-height: 50px;
+}
+
 </style>
