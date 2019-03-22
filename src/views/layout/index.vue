5d11278a1a6f2c1dd471253bb93b517d319fdb12
sasonglin <sasonglinzb@163.com>
2019-03-22 16:28:01 +0800

    设置layout模块

---
+++ src/views/layout/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content">
+    <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.content {
+  height: 100%;
+}
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color:  #b3c0d1;
+}
+.el-main {
+  background-color: #e9eef3;
+}
+.el-aside {
+  background-color: #d3dce6;
+}
+</style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="login-wrap">
+    <div class="main-login">
+      <el-form label-width="80px" :rules="LoginFormRules" :model="loginForm" ref="loginFormEl">
+        <el-form-item label="用户名" prop="username">
+          <el-input  v-model='loginForm.username'></el-input>
+        </el-form-item>
+        <el-form-item label="密 码" prop="password">
+          <el-input v-model='loginForm.password' show-password></el-input>
+        </el-form-item>
+        <el-button class="btn" type="primary" @click.prevent="handleLogin">登 录</el-button>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      LoginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    async handleLogin () {
+      this.$refs.loginFormEl.validate(valid => {
+        if (valid) {
+          this.Login()
+        } else {
+          return false
+        }
+      })
+    },
+    async Login () {
+      const loginForm = this.loginForm
+      const { meta } = (await axios.post('http://localhost:8888/api/private/v1/login', loginForm)).data
+      if (meta.status === 200) {
+        this.checkTure(meta)
+      } else {
+        this.checkFalse(meta)
+      }
+    },
+    checkTure (meta) {
+      this.$message({
+        message: meta.msg,
+        type: 'success'
+      })
+    },
+    checkFalse (meta) {
+      this.$message.error('登录失败：' + meta.msg)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-wrap {
+  height: 100%;
+  background-color: #2f4050;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.main-login {
+  width: 400px;
+  height: 230px;
+  border-radius: 20px;
+  background: #fff;
+  box-sizing: border-box;
+  padding: 30px 50px;
+}
+
+.btn {
+  width: 100%;
+  font-size: 20px;
+}
+</style>
